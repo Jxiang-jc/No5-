f2ee37f6b2bcabc432d73a4efe390860283af432
Jxiang-jc <zjx1020645@163.com>
2018-08-19 12:06:14 +0800

    更新一下

--- src/js/index.js
+++ src/js/index.js
@@ -12,6 +12,35 @@
     let times = $hour>=12 ? '下午好,':'上午好,';
 
     //写进页面
-    $span.html(times)
+    $span.html(times);
+
     })
+})();
+//头部搜索
+(function(){
+    jQuery(function($){
+        //获取inpu边框
+        var $input = $('#searchInput');
+        var val = $input.val();
+
+        //鼠标移动事件
+        $input.on('mouseover',function(){
+
+            $input.addClass('inputBgc');
+            
+            $('#pageHeader_b .container .col-lg-6 .z_delSearch').css('visibility',"visible");
+            
+        })
+        $input.on('click',function(){
+             $input.val('');
+
+        })
+        $input.on('blur',function(){
+            
+            console.log(val)
+            // $input.val('面膜');
+        })
+
+    })
+
 })();
